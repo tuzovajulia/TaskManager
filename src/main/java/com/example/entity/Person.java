0e85bb6ccbbeb80e1f51052474c95e8c694915f6
julia <tuzovajull@gmail.com>
2025-08-11 20:02:40 +0300

    class Person

---
+++ src/main/java/com/example/entity/Person.java
@@ -0,0 +1,75 @@
+package com.example.entity;
+
+import java.util.List;
+
+public class Person {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private List<Task> tasks;
+
+    public Person() {
+
+    }
+
+    public Person(int id, String firstName, String lastName, String email,
+                  List<Task> tasks) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.tasks = tasks;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", tasks=" + tasks +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
