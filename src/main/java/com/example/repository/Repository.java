5a0ba9fa607c322698e85d4187f1d4dd2a50a06c
julia <tuzovajull@gmail.com>
2025-08-12 22:13:39 +0300

    написала интерфейс Repository

---
+++ src/main/java/com/example/repository/Repository.java
@@ -0,0 +1,16 @@
+package com.example.repository;
+
+import java.util.List;
+
+public interface Repository<T> {
+    void create(T entity);
+
+    T read(int id);
+
+    void update(T entity);
+
+    void delete(int id);
+
+    List<T> findAll();
+
+}
