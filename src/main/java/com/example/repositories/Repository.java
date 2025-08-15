da58d32804da2a3ba1c9d2fa30c48166c979aefe
julia <tuzovajull@gmail.com>
2025-08-15 16:42:15 +0300

    создала интерфейс

---
+++ src/main/java/com/example/repositories/Repository.java
@@ -0,0 +1,11 @@
+package com.example.repositories;
+
+import java.util.List;
+
+public interface Repository<T> {
+    void create(T entity);
+    T read(int id);
+    void update(T entity);
+    void delete(int id);
+    List<T> findAll();
+}
