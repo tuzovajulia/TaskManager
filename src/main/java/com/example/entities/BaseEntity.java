3e4195acb2b9f506d822dcb4db8830387184c9cb
julia <tuzovajull@gmail.com>
2025-08-15 15:37:41 +0300

    добавила новый класс

---
+++ src/main/java/com/example/entities/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.example.entities;
+
+public class BaseEntity {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
