dfed3ca9ea0a1f7ded27c49526a758cb25bf9b23
julia <tuzovajull@gmail.com>
2025-08-22 10:22:17 +0300

    добавила интерфейс

--- src/main/java/com/example/repositories/PersonRepository.java
+++ src/main/java/com/example/repositories/PersonRepository.java
@@ -2,7 +2,7 @@ package com.example.repositories;
 
 import com.example.entities.Person;
 
-public class PersonRepository extends AbstractRepository<Person> {
+public class PersonRepository extends AbstractRepository<Person> implements Repository <Person> {
     private static PersonRepository instance;
 
     private PersonRepository() {
