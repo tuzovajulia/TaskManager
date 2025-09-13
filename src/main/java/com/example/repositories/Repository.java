a352b5e8a825243417f2adcb2e5576a051c65008
julia <tuzovajull@gmail.com>
2025-09-14 01:53:26 +0300

    добавила UUID

--- src/main/java/com/example/repositories/Repository.java
+++ src/main/java/com/example/repositories/Repository.java
@@ -1,11 +1,12 @@
 package com.example.repositories;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface Repository<T> {
     void create(T entity);
-    T read(int id);
+    T read(UUID id);
     void update(T entity);
-    void delete(int id);
+    void delete(UUID id);
     List<T> getAll();
 }
